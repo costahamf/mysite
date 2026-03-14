8feb7ac3273d9fd43cb90caedb0d0d4283a78583
costahamf <114944745+costahamf@users.noreply.github.com>
2026-03-14 19:01:13 +0300

    Delete dashboard directory

--- dashboard/delete_courier.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/../config/init.php';
-requireAuth();
-
-http_response_code(403);
-exit('Удаление курьеров доступно только администратору.');
